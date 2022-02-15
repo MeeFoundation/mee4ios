dc2c4ff8c935eb248c85a955d89229347f8e8812
AlexSloMee <alexslo9219@gmail.com>
2022-02-15 11:27:00 +0300

    Initial commit

---
+++ Shared/mee_ios_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  mee_ios_clientApp.swift
+//  Shared
+//
+//  Created by alex slobodeniuk on 15.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct mee_ios_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
