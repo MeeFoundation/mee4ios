d64551ff2c38a50791255708ac44f7f7a329a135
AlexSloMee <alexslo9219@gmail.com>
2022-02-18 13:15:47 +0300

    Add extension to ios app + new deeplinks config

---
+++ Mee Safari Extension/Resources/content.js
@@ -0,0 +1,14 @@
+browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
+    console.log("Received response: ", response);
+});
+
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log("Received request: ", request);
+});
+
+const injectedCode = `
+window.meeAppLink = "mee-client://"
+`
+var script = document.createElement("script");
+script.textContent = injectedCode;
+(document.head).appendChild(script);
---
+++ Mee Safari Extension/Resources/popup.js
@@ -0,0 +1,1 @@
+console.log("Hello Mee Extension!", browser);
