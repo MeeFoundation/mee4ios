da0df54481bc197bfefeace85a4224d0be371d61
Anthony Ivanov <aivanov@swiftinvention.com>
2022-12-15 10:33:58 +0100

    keychain removed

--- ios-app/Shared/helpers/MeeAgentStore.swift
+++ ios-app/Shared/helpers/MeeAgentStore.swift
@@ -9,82 +9,82 @@ import Foundation
 import KeychainAccess
 import AuthenticationServices
 
-class MeeAgentStore {
-    private let store = ASCredentialIdentityStore.shared
-    init() {
-        keychain = Keychain(service: "org.getmee.ios-client", accessGroup: "59TG46326T.org.getmee.ios-client.connections").synchronizable(true)
-    }
-    private var keychain: Keychain
-
-    func removeItembyName (name: String) -> Void {
-        do {
-            try keychain.remove(name)
-            return
-        } catch {
-            return
-        }
-    }
-
-    func getItemByName (name: String) -> String? {
-        do {
-            guard let oneItem = try keychain.get(name) else {
-                return nil
-            }
-            return oneItem
-        } catch {
-            return nil
-        }
-
-    }
-
-    func editItem (name: String, item: String) {
-        do {
-            try keychain.set(item, key: name)
-        } catch {
-
-        }
-
-    }
-}
-
-//
-//
 //class MeeAgentStore {
-//    private let agent: MeeAgent
+//    private let store = ASCredentialIdentityStore.shared
 //    init() {
-//        agent = getAgent(databaseUrl: Bundle.main.url(forResource: "mee", withExtension: "sqlite")!.relativeString);
-//
+//        keychain = Keychain(service: "org.getmee.ios-client", accessGroup: "59TG46326T.org.getmee.ios-client.connections").synchronizable(true)
 //    }
+//    private var keychain: Keychain
 //
-//    func removeItembyName (name: String) -> String? {
+//    func removeItembyName (name: String) -> Void {
 //        do {
-//            try agent.deleteCtx(ctxId: name)
-//            return name
+//            try keychain.remove(name)
+//            return
 //        } catch {
-//            return nil
+//            return
 //        }
 //    }
 //
 //    func getItemByName (name: String) -> String? {
 //        do {
-//            guard let oneItem = try agent.getCtx(ctxId: name)?.data else {
+//            guard let oneItem = try keychain.get(name) else {
 //                return nil
 //            }
 //            return oneItem
 //        } catch {
 //            return nil
 //        }
+//
 //    }
 //
-//    func editItem (name: String, item: String) -> DsContainer? {
+//    func editItem (name: String, item: String) {
 //        do {
-//            if (getItemByName(name: name)) != nil {
-//                self.removeItembyName(name: name)
-//            }
-//            return try agent.createCtx(ctxId: name, ctxName: name, ctxData: item)
-//        } catch let e {
-//            print("error: ", e)
-//            return nil
+//            try keychain.set(item, key: name)
+//        } catch {
+//
 //        }
+//
 //    }
 //}
+
+
+
+class MeeAgentStore {
+    private let agent: MeeAgent
+    init() {
+        agent = getAgent(databaseUrl: Bundle.main.url(forResource: "mee", withExtension: "sqlite")!.relativeString);
+
+    }
+
+    func removeItembyName (name: String) -> String? {
+        do {
+            try agent.deleteCtx(ctxId: name)
+            return name
+        } catch {
+            return nil
+        }
+    }
+
+    func getItemByName (name: String) -> String? {
+        do {
+            guard let oneItem = try agent.getCtx(ctxId: name)?.data else {
+                return nil
+            }
+            return oneItem
+        } catch {
+            return nil
+        }
+    }
+
+    func editItem (name: String, item: String) -> DsContainer? {
+        do {
+            if (getItemByName(name: name)) != nil {
+                self.removeItembyName(name: name)
+            }
+            return try agent.createCtx(ctxId: name, ctxName: name, ctxData: item)
+        } catch let e {
+            print("error: ", e)
+            return nil
+        }
+    }
+}
